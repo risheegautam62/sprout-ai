9702322513ecd75421ae8894b22a0e84212bb338
risheegautam62 <0112cs201090@gmail.com>
2024-09-24 14:47:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+$(document).ready(function() {
+    $(".hamburger").click(function() {
+        $("#mySidebar").addClass("open");
+        $("body").addClass("sidebar-open");
+    });
+    $(".closebtn").click(function() {
+        $("#mySidebar").removeClass("open");
+        $("body").removeClass("sidebar-open");
+    });
+    $(document).click(function(event) {
+        if (!$(event.target).closest("#mySidebar, .hamburger").length && $("#mySidebar").hasClass("open")) {
+            $("#mySidebar").removeClass("open");
+            $("body").removeClass("sidebar-open");
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000000,
+        infinite: true,
+        nextArrow: $('.next-button'),
+        prevArrow: $('.prev-button'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false, 
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false, 
+                    infinite: true
+                }
+            }
+        ]
+    });
+});
+
